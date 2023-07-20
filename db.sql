21ebeb674b4b5eec9f619b4293cf6524336a81e8
Prat55 <pratikdesai9059@gmail.com>
2023-07-20 13:32:48 +0530

    changes

--- db.sql
+++ db.sql
@@ -8,7 +8,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `rosh-credentials` (
   `email` varchar(191) NOT NULL,
   `password` varchar(191) NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=latin7_general_cs;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 INSERT INTO `rosh-credentials` (`email`, `password`) VALUES ('admin@gmail.com', 'PassAdmin@90');
@@ -20,91 +20,103 @@ CREATE TABLE `category1` (
   `id` int(11) NOT NULL,
   `service` varchar(191) NOT NULL,
   `price` varchar(191) NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=latin7_general_cs;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `category1` (`id`,`service`, `price`) VALUES (1,'Eyebrow', '30');
-INSERT INTO `category1` (`id`,`service`, `price`) VALUES (2,'Forehead and Uperlips', '50');
+INSERT INTO `category1` (`id`,`service`, `price`) 
+VALUES 
+(1,'Eyebrow', '30'),
+(2,'Forehead and Uperlips', '50');
 
 
 CREATE TABLE `category2` (
   `id` int(11) NOT NULL,
   `service` varchar(191) NOT NULL,
   `price` varchar(191) NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=latin7_general_cs;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `category2` (`id`,`service`, `price`) VALUES (1,'Hand Wax', '150');
-INSERT INTO `category2` (`id`,`service`, `price`) VALUES (2,'Underarms', '50');
-INSERT INTO `category2` (`id`,`service`, `price`) VALUES (3,'Body Wax', '600');
-INSERT INTO `category2` (`id`,`service`, `price`) VALUES (4,'leg Wax(Half)', '200');
-INSERT INTO `category2` (`id`,`service`, `price`) VALUES (5,'Leg Wax(Full)', '350');
-INSERT INTO `category2` (`id`,`service`, `price`) VALUES (6,'Face Wax', '100');
+INSERT INTO `category2` (`id`,`service`, `price`) 
+VALUES 
+(1,'Hand Wax', '150'),
+(2,'Underarms', '50'),
+(3,'Body Wax', '600'),
+(4,'leg Wax(Half)', '200'),
+(5,'Leg Wax(Full)', '350'),
+(6,'Face Wax', '100');
 
 
 CREATE TABLE `category3` (
   `id` int(11) NOT NULL,
   `service` varchar(191) NOT NULL,
   `price` varchar(191) NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=latin7_general_cs;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `category3` (`id`,`service`, `price`) VALUES (1,'Hand Wax', '100');
-INSERT INTO `category3` (`id`,`service`, `price`) VALUES (2,'Underarms', '300');
-INSERT INTO `category3` (`id`,`service`, `price`) VALUES (3,'leg Wax(Half)', '500');
-INSERT INTO `category3` (`id`,`service`, `price`) VALUES (4,'Leg Wax(Full)', '800');
-INSERT INTO `category3` (`id`,`service`, `price`) VALUES (5,'Body Wax', '1200');
-INSERT INTO `category3` (`id`,`service`, `price`) VALUES (6,'Face Wax', '200');
+INSERT INTO `category3` (`id`,`service`, `price`) 
+VALUES 
+(1,'Hand Wax', '100'),
+(2,'Underarms', '300'),
+(3,'leg Wax(Half)', '500'),
+(4,'Leg Wax(Full)', '800'),
+(5,'Body Wax', '1200'),
+(6,'Face Wax', '200');
 
 
 CREATE TABLE `category4` (
   `id` int(11) NOT NULL,
   `service` varchar(191) NOT NULL,
   `price` varchar(191) NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=latin7_general_cs;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `category4` (`id`,`service`, `price`) VALUES (1,'Clean Up(Fruit)', '200');
-INSERT INTO `category4` (`id`,`service`, `price`) VALUES (2,'Fruit Facial', '400');
-INSERT INTO `category4` (`id`,`service`, `price`) VALUES (3,'Lotus Facial', '800');
-INSERT INTO `category4` (`id`,`service`, `price`) VALUES (4,'RichFeel Facial', '1000');
-INSERT INTO `category4` (`id`,`service`, `price`) VALUES (5,'Cheryls Facial', '800');
-INSERT INTO `category4` (`id`,`service`, `price`) VALUES (6,'O3', '1500-2000');
-INSERT INTO `category4` (`id`,`service`, `price`) VALUES (7,'Raga Facial', '700');
-INSERT INTO `category4` (`id`,`service`, `price`) VALUES (8,'Oxyglow', '500');
-INSERT INTO `category4` (`id`,`service`, `price`) VALUES (9,'VLCC', '600');
+INSERT INTO `category4` (`id`,`service`, `price`) 
+VALUES 
+(1,'Clean Up(Fruit)', '200'),
+(2,'Fruit Facial', '400'),
+(3,'Lotus Facial', '800'),
+(4,'RichFeel Facial', '1000'),
+(5,'Cheryls Facial', '800'),
+(6,'O3', '1500-2000'),
+(7,'Raga Facial', '700'),
+(8,'Oxyglow', '500'),
+(9,'VLCC', '600');
 
 
 CREATE TABLE `images` (
   `id` int(11) NOT NULL,
   `file_name` varchar(191) NOT NULL,
   `uploaded_on` datetime NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=latin7_general_cs;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (1,'1.jfif');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (2,'2.jfif');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (3,'4.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (4,'5.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (5,'6.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (6,'7.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (7,'9.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (8,'10.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (9,'11.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (10,'12.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (11,'13.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (12,'14.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (13,'15.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (14,'16.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (15,'17.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (16,'18.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (17,'19.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (18,'20.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (19,'21.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (20,'22.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (21,'23.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (22,'24.jpg');
-INSERT INTO `images` (`id`,`service`, `price`) VALUES (23,'25.jpg');
+INSERT INTO `images` (`id`,`service`) 
+VALUES 
+(1,'1.jfif'),
+(2,'2.jfif'),
+(3,'4.jpg'),
+(4,'5.jpg'),
+(5,'6.jpg'),
+(6,'7.jpg'),
+(7,'9.jpg'),
+(8,'10.jpg'),
+(9,'11.jpg'),
+(10,'12.jpg'),
+(11,'13.jpg'),
+(12,'14.jpg'),
+(13,'15.jpg'),
+(14,'16.jpg'),
+(15,'17.jpg'),
+(16,'18.jpg'),
+(17,'19.jpg'),
+(18,'20.jpg'),
+(19,'21.jpg'),
+(20,'22.jpg'),
+(21,'23.jpg'),
+(22,'24.jpg'),
+(23,'25.jpg');
+
 --
 -- Indexes for table `category1`
 --
 ALTER TABLE `category1`
   ADD PRIMARY KEY (`id`);
+
 
 --
 -- Indexes for table `category2`
@@ -112,17 +124,20 @@ ALTER TABLE `category1`
 ALTER TABLE `category2`
   ADD PRIMARY KEY (`id`);
 
+
 --
 -- Indexes for table `category3`
 --
 ALTER TABLE `category3`
   ADD PRIMARY KEY (`id`);
 
+
 --
 -- Indexes for table `category4`
 --
 ALTER TABLE `category4`
   ADD PRIMARY KEY (`id`);
+
 
 --
 -- Indexes for table `images`
