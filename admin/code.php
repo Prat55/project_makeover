f877e571ea54a37e0f60d0cac423cb1914de1af1
prat55 <pratikdesai9059@gmail.com>
2024-08-21 18:45:23 +0530

    changes

--- admin/code.php
+++ admin/code.php
@@ -1,48 +1,41 @@
 <?php
-    session_start();
-    include('../functions/myfunction.php');
+session_start();
+include('../functions/myfunction.php');
 
-    // * Adding Data in 1st Category
-    if(isset($_POST['add_category'])){
-        $category = $_POST['category'];
+// * Adding Data in 1st Category
+if (isset($_POST['add_category'])) {
+    $category = $_POST['category'];
 
-        resetQuery("categories");
-        
-        $query = "INSERT INTO `categories`(`category`) VALUES ('$category')";
-        $result = mysqli_query($con, $query);
-        
-        if($result){
-            redirect("add-category.php", "success","Category Added Successfully");
-        }
-        else{
-            redirect("add-category.php", "error", "Category Not Added");
-        }
-    }
-    else if(isset($_POST['add_subcategory'])){
-        $category_id = $_POST['category_id'];
-        $service = $_POST['service'];
-        $price = $_POST['price'];
+    resetQuery("categories");
 
-        resetQuery("ratecard");
-        $query = "INSERT INTO `ratecard`(`category_id`, `service`, `price`) VALUES ('$category_id','$service','$price')";
-        $result = mysqli_query($con, $query);
-        
-        if($result){
-            redirect("add-subcategory.php", "success", "Category Added Successfully");
-        }
-        else{
-            redirect("add-subcategory.php", "error", "Category Not Added");
-        }
-    }
-    // * Update data in category tables
-    else if(isset($_POST['update_category_btn'])){
-        updateCategory("categories");
-    }
-    else if(isset($_POST['update_sub_category_btn'])){
-        updateSubCategory("ratecard");
-    }
-    else if(isset($_POST['add_course'])){
-        addCources("courses");
-    }
+    $query = "INSERT INTO `categories`(`category`) VALUES ('$category')";
+    $result = mysqli_query($con, $query);
 
-?>
+    if ($result) {
+        redirect("add-category.php", "success", "Category Added Successfully");
+    } else {
+        redirect("add-category.php", "error", "Category Not Added");
+    }
+} else if (isset($_POST['add_subcategory'])) {
+    $category_id = $_POST['category_id'];
+    $service = $_POST['service'];
+    $price = $_POST['price'];
+
+    resetQuery("ratecard");
+    $query = "INSERT INTO `ratecard`(`category_id`, `service`, `price`) VALUES ('$category_id','$service','$price')";
+    $result = mysqli_query($con, $query);
+
+    if ($result) {
+        redirect("add-subcategory.php", "success", "Category Added Successfully");
+    } else {
+        redirect("add-subcategory.php", "error", "Category Not Added");
+    }
+}
+// * Update data in category tables
+else if (isset($_POST['update_category_btn'])) {
+    updateCategory("categories");
+} else if (isset($_POST['update_sub_category_btn'])) {
+    updateSubCategory("ratecard");
+} else if (isset($_POST['add_course'])) {
+    addCources("courses");
+}
--- config/dbcon.php
+++ config/dbcon.php
@@ -4,5 +4,3 @@ $con = mysqli_connect("localhost", "root", "", "rosh_makeover");
 if (!$con) {
     die('Connection Failed' . mysqli_connect_error());
 }
-
-?>
